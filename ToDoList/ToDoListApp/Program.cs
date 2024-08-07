a6b85a114a21b5b3dd6dbc83031b37c58a592bca
Madelene Axblom <90625377+GreenGard@users.noreply.github.com>
2024-08-07 19:58:19 +0200

    Add ToDoList, will update with input from user

---
+++ ToDoList/ToDoListApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> tasks = new List<string>(); // En lista av strängar
+            
+            // Lägg till några uppgifter
+            tasks.Add("Simma");
+            tasks.Add("Springa");
+            tasks.Add("Leka");
+
+            // Visa uppgifterna
+            foreach (string task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
